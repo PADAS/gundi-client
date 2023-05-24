62286557de3b10e317c4d55ba0340435701f1c9a
Victor H. García <victorhugogarcia13d@gmail.com>
2023-05-24 00:09:00 -0600

    First approach of aiohttp replacement for httpx

--- gundi_client/client.py
+++ gundi_client/client.py
@@ -2,7 +2,6 @@ import logging
 from datetime import datetime, timedelta, timezone
 from typing import List, Dict, Any
 from uuid import UUID
-from aiohttp import ClientSession, ClientResponseError, ClientTimeout
 from pydantic import parse_obj_as
 from . import settings
 from .schemas import (
@@ -12,6 +11,7 @@ from .schemas import (
     DeviceState,
     OutboundConfiguration,
 )
+from httpx import AsyncClient, RequestError
 
 logger = logging.getLogger(__name__)
 logger.setLevel(settings.LOG_LEVEL)
@@ -33,8 +33,9 @@ class PortalApi:
 
         self.cached_token = None
         self.cached_token_expires_at = datetime.min.replace(tzinfo=timezone.utc)
+        self.session = AsyncClient(timeout=120, verify=settings.CDIP_ADMIN_SSL_VERIFY)
 
-    async def get_access_token(self, session: ClientSession) -> OAuthToken:
+    async def get_access_token(self) -> OAuthToken:
         if self.cached_token and self.cached_token_expires_at > datetime.now(
             tz=timezone.utc
         ):
@@ -51,12 +52,12 @@ class PortalApi:
             "scope": "openid",
         }
 
-        response = await session.post(
-            self.oauth_token_url, data=payload, ssl=settings.CDIP_ADMIN_SSL_VERIFY
+        response = await self.session.post(
+            self.oauth_token_url, data=payload
         )
 
         response.raise_for_status()
-        token = await response.json()
+        token = response.json()
         token = OAuthToken.parse_obj(token)
         self.cached_token_expires_at = datetime.now(tz=timezone.utc) + timedelta(
             seconds=token.expires_in - 15
@@ -64,8 +65,8 @@ class PortalApi:
         self.cached_token = token
         return token
 
-    async def get_auth_header(self, session: ClientSession) -> dict:
-        token_object = await self.get_access_token(session)
+    async def get_auth_header(self) -> dict:
+        token_object = await self.get_access_token()
         return {
             "authorization": f"{token_object.token_type} {token_object.access_token}"
         }
@@ -73,39 +74,34 @@ class PortalApi:
     async def get_destinations(
         self, *args, integration_id: str = None, device_id: str = None
     ):
-        async with ClientSession() as session:
-            headers = await self.get_auth_header(session)
+        headers = await self.get_auth_header()
 
-            url = f"{self.portal_api_endpoint}/integrations/outbound/configurations"
-            response = await session.get(
-                url,
-                params={"inbound_id": integration_id, "device_id": device_id},
-                headers=headers,
-            )
+        url = f"{self.portal_api_endpoint}/integrations/outbound/configurations"
+        response = await self.session.get(
+            url,
+            params={"inbound_id": integration_id, "device_id": device_id},
+            headers=headers,
+        )
 
-            if response.ok:
-                data = await response.json()
+        response.raise_for_status()
+        data = response.json()
 
-                return [OutboundConfiguration.parse_obj(item) for item in data]
-
-            return []
+        return [OutboundConfiguration.parse_obj(item) for item in data]
 
     async def get_authorized_integrations(
         self,
-        session: ClientSession,
         t_int_info: TIntegrationInformation = IntegrationInformation,
     ) -> List[IntegrationInformation]:
         logger.debug(f"get_authorized_integrations for : {settings.KEYCLOAK_CLIENT_ID}")
-        headers = await self.get_auth_header(session)
+        headers = await self.get_auth_header()
 
         logger.debug(f"url: {self.integrations_endpoint}")
-        response = await session.get(
+        response = await self.session.get(
             url=self.integrations_endpoint,
             headers=headers,
-            ssl=settings.CDIP_ADMIN_SSL_VERIFY,
         )
         response.raise_for_status()
-        json_response = await response.json()
+        json_response = response.json()
 
         if isinstance(json_response, dict):
             json_response = [json_response]
@@ -116,40 +112,36 @@ class PortalApi:
         return [t_int_info.parse_obj(r) for r in json_response]
 
     async def update_state(
-        self, session: ClientSession, integration_info: IntegrationInformation
+        self, integration_info: IntegrationInformation
     ):
-        async with ClientSession() as sess:
-            headers = await self.get_auth_header(sess)
+        headers = await self.get_auth_header()
 
-            response = await sess.put(
-                url=f"{self.integrations_endpoint}/{integration_info.id}",
-                headers=headers,
-                json=dict(state=integration_info.state),
-                ssl=settings.CDIP_ADMIN_SSL_VERIFY,
-            )
-            logger.info(f"update integration state resp: {response.status}")
-            response.raise_for_status()
+        response = await self.session.put(
+            url=f"{self.integrations_endpoint}/{integration_info.id}",
+            headers=headers,
+            json=dict(state=integration_info.state),
+        )
+        logger.info(f"update integration state resp: {response.json()}")
+        response.raise_for_status()
 
-            return await self.update_states_with_dict(
-                sess, integration_info.id, integration_info.device_states
-            )
+        return await self.update_states_with_dict(
+            self.session, integration_info.id, integration_info.device_states
+        )
 
-    async def fetch_device_states(self, session: ClientSession, inbound_id: UUID):
+    async def fetch_device_states(self, inbound_id: UUID):
         try:
-            headers = await self.get_auth_header(session)
+            headers = await self.get_auth_header()
 
             # This ought to be quick so just do it straight away.
-            response = await session.get(
+            response = await self.session.get(
                 url=f"{self.device_states_endpoint}/",
                 params={"inbound_config_id": str(inbound_id)},
                 headers=headers,
-                timeout=ClientTimeout(sock_connect=3.1, sock_read=10),
-                verify=settings.CDIP_ADMIN_SSL_VERIFY,
             )
-            if response.status == 200:
-                result = response.json()
-        except ClientResponseError as ex:
-            logger.exception("Failed to get devices for iic: {inbound_id}")
+            response.raise_for_status()
+            result = response.json()
+        except RequestError:
+            logger.exception(f"Failed to get devices for iic: {inbound_id}")
         else:
             states_received = parse_obj_as(List[DeviceState], result)
             # todo: cleanup after all functions have their device state migrated over
@@ -163,84 +155,78 @@ class PortalApi:
             # return {s.device_external_id: s.state for s in states_received}
 
     async def ensure_device(
-        self, session: ClientSession, inbound_id: UUID, external_id: str
+        self, inbound_id: UUID, external_id: str
     ):
-        # Post device ID and Integration ID combination to ensure it exists
-        # in the Portal's database and is also in the Integration's default
-        # device group.
-        headers = await self.get_auth_header(session)
-        payload = {"external_id": external_id, "inbound_configuration": inbound_id}
-        response = await session.post(
-            url=self.devices_endpoint,
-            json=payload,
-            headers=headers,
-            ssl=settings.CDIP_ADMIN_SSL_VERIFY,
-        )
-        resp = await response.json()
-        # print(resp)
-        if response.ok:
+        try:
+            # Post device ID and Integration ID combination to ensure it exists
+            # in the Portal's database and is also in the Integration's default
+            # device group.
+            headers = await self.get_auth_header()
+            payload = {"external_id": external_id, "inbound_configuration": inbound_id}
+            response = await self.session.post(
+                url=self.devices_endpoint,
+                json=payload,
+                headers=headers,
+            )
+            # print(resp)
+            response.raise_for_status()
+            resp = response.json()
             return resp
-        else:
-            logger.error(
+        except Exception:
+            logger.exception(
                 "Failed to post device to portal.",
-                extra={**payload, "response": str(response)},
+                extra={**payload},
             )
 
         return None
 
     async def update_states_with_dict(
-        self, session: ClientSession, inbound_id: UUID, states_dict: Dict[str, Any]
+        self, inbound_id: UUID, states_dict: Dict[str, Any]
     ):
-        headers = await self.get_auth_header(session)
-        response = await session.post(
+        headers = await self.get_auth_header()
+        response = await self.session.post(
             url=f"{self.device_states_endpoint}/update/{inbound_id}",
             headers=headers,
             json=states_dict,
-            ssl=settings.CDIP_ADMIN_SSL_VERIFY,
         )
         response.raise_for_status()
-        text = await response.text()
-        logger.info(f"update device_states resp: {response.status}")
+        text = response.json()
+        logger.info(f"update device_states resp: {response.json()}")
         return text
 
-    async def _get(self, session: ClientSession, url: str, params=None):
-        headers = await self.get_auth_header(session)
-        response = await session.get(
+    async def _get(self, url: str, params=None):
+        headers = await self.get_auth_header()
+        response = await self.session.get(
             url=url,
             headers=headers,
             params=params,
-            ssl=settings.CDIP_ADMIN_SSL_VERIFY,
         )
         response.raise_for_status()
-        return await response.json()
+        return response.json()
 
-    async def get_bridge_integration(self, session: ClientSession, bridge_id: str):
+    async def get_bridge_integration(self, bridge_id: str):
         return await self._get(
-            session=session,
             url=f"{settings.PORTAL_API_ENDPOINT}/integrations/bridges/{bridge_id}",
         )
 
     async def get_inbound_integration(
-        self, session: ClientSession, integration_id: str
+        self, integration_id: str
     ):
         return await self._get(
-            session=session,
             url=f"{settings.PORTAL_API_ENDPOINT}/integrations/inbound/configurations/{integration_id}",
         )
 
     async def get_outbound_integration(
-        self, session: ClientSession, integration_id: str
+        self, integration_id: str
     ):
         return await self._get(
-            session=session,
             url=f"{settings.PORTAL_API_ENDPOINT}/integrations/outbound/configurations/{integration_id}",
         )
 
     async def get_outbound_integration_list(
-        self, session: ClientSession, **query_params
+        self, **query_params
     ):
         return await self._get(
-            session=session,
             url=f"{settings.PORTAL_API_ENDPOINT}/integrations/outbound/configurations",
             params=query_params,
         )
