65b965d0e92539c912719c5d2ad6c11022809e29
Victor H. García <victorhugogarcia13d@gmail.com>
2024-07-15 17:10:35 -0600

    Refactor for including support for file names in event attachments

--- gundi_client_v2/client.py
+++ gundi_client_v2/client.py
@@ -36,10 +36,10 @@ class GundiDataSenderClient:
     async def post_events(self, data: List[dict]) -> dict:
         return await self._post_data(data=data, endpoint="events")
 
-    async def post_event_attachments(self, event_id: str, attachments: List[bytes]) -> dict:
+    async def post_event_attachments(self, event_id: str, attachments: List[tuple]) -> dict:
         return await self._post_data(attachments=attachments, endpoint=f"events/{event_id}/attachments")
 
-    async def _post_data(self, data: List[dict] = None, endpoint: str = None, attachments: List[bytes] = None) -> dict:
+    async def _post_data(self, data: List[dict] = None, endpoint: str = None, attachments: List[tuple] = None) -> dict:
         apikey = self._api_key
 
         logger.info(
@@ -59,7 +59,9 @@ class GundiDataSenderClient:
             request["json"] = clean_batch
 
         if attachments:
-            request["files"] = [('file', image_binary) for image_binary in attachments]
+            request["files"] = [
+                ('file', (filename, image_binary)) for filename, image_binary in attachments
+            ]
 
         logger.debug(
             f" -- sending {endpoint}. --",
